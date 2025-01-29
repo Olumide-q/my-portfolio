6246beabd0ee7f261eac9024f4d563e2feddbdcb
APPLE <apple@APPLEs-MacBook-Pro-2.local>
2025-01-29 16:12:00 +0100

    third commit

--- src/reusable/SkillsCardBox.jsx
+++ src/reusable/SkillsCardBox.jsx
@@ -3,9 +3,9 @@ import { skills } from '../Db'
 
 const SkillsCardBox = () => {
   return (
-    <div className='grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 gap-8 md:gap-6 w-full max-w-6xl'>
+    <div className='grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 gap-8 md:gap-6 w-full'>
       {skills.map((skill)=>(
-        <div className='w-[330px] md:w-[340px] lg:w-[270px] flex justify-center items-center bg-sky-50 h-60 rounded shadow-sm hover:border-b-4 rounded-b-md border-purple-700 px-4 ' key={skill.id}>
+        <div className='w-[330px] md:w-[340px] lg:w-full flex justify-center items-center bg-sky-50 h-60 rounded shadow-sm hover:border-b-4 rounded-b-md border-purple-700 px-4 ' key={skill.id}>
           <div className='flex flex-col gap-6 '>
           <div className='bg-white p-4 w-16 h-16 flex justify-center rounded-md '>
             <img src={skill.icon} alt={skill.name}/>
